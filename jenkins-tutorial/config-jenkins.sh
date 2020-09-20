7dbb9ce8fb7a0736cf11b91f677c0337cf9f5959
Nenad Miladinovic <nenadmiladin@yahoo.com>
2020-09-20 10:32:33 +0200

    Update config-jenkins.sh

--- jenkins-tutorial/config-jenkins.sh
+++ jenkins-tutorial/config-jenkins.sh
@@ -2,6 +2,7 @@
 
 # Jenkins
 sudo wget -qO - https://pkg.jenkins.io/debian-stable/jenkins.io.key | apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
 sudo apt-get update
 sudo apt install openjdk-8-jdk
